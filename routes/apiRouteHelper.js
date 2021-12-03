74cfd84232519a1cc081f179f533c0d6d33d8598
pardue95 <85760640+pardue95@users.noreply.github.com>
2021-12-03 17:27:18 -0600

    working on routes

---
+++ routes/apiRouteHelper.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const apiRoutes = require('..routes/apiRoutes/apiRoutes.js');
+
+router.use(apiRoutes);
+
+module.exports = router;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
-const { notes } = require('../../db/db');
+// const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
+const { notes } = require('../db/db.json');
 
-router.get('/api/notes', (req, res) => {
-    let results = notes;
-    if (req.query) {
-      results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-  });
+router.get('/api/notes', function(req, res) {
+  return res.sendFile(path.json(__dirname, "db/db.json"));
+});
+  module.exports = router;
